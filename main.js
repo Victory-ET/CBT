c1adebc4537e5b4fc8eb713b3160465eac9ea940
miracleonyenma <miracleficient@gmail.com>
2019-10-05 16:43:15 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the javascript",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer:1
+    },
+    {
+        question: "what is 2*2?",
+        choice1: "1",
+        choice2: "22",
+        choice3: "4",
+        choice4: "four",
+        answer:3
+
+    },
+    {
+        question: "what is the square of 12?",
+        choice1: "12.dotproduct.12",
+        choice2: "14.4",
+        choice3: "14four",
+        choice4: "144",
+        answer:4
+    }
+];
+//constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [... questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+    if(availableQuestions.length ==0 || questionCounter > MAX_QUESTIONS){
+        //go to the end page
+        return window.location.assign("./end.html");
+    }
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random()* availableQuestions.length );
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e =>{
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        //console.log(classToApply);
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+       // console.log(selectedAnswer == currentQuestion.answer);
+
+    });
+});
+
+startGame();
+
+   
